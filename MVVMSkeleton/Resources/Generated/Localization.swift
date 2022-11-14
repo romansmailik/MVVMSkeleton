23e156e5b7b424908da5b927396440a9103ed1ef
Roman Savchenko <roman.savchenko@trackimo.com>
2022-11-14 23:02:50 +0200

    added xcodegen config

--- MVVMSkeleton/Resources/Generated/Localization.swift
+++ MVVMSkeleton/Resources/Generated/Localization.swift
@@ -3,7 +3,7 @@
 
 import Foundation
 
-// swiftlint:disable superfluous_disable_command file_length implicit_return
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
 
 // MARK: - Strings
 
@@ -11,35 +11,38 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum Localization {
   /// Cancel
-  internal static let cancel = Localization.tr("Localizable", "cancel")
+  internal static let cancel = Localization.tr("Localizable", "cancel", fallback: "Cancel")
   /// Choose option
-  internal static let chooseOption = Localization.tr("Localizable", "choose-option")
+  internal static let chooseOption = Localization.tr("Localizable", "choose-option", fallback: "Choose option")
   /// Confirm password
-  internal static let confirmPassword = Localization.tr("Localizable", "confirm-password")
+  internal static let confirmPassword = Localization.tr("Localizable", "confirm-password", fallback: "Confirm password")
   /// Done
-  internal static let done = Localization.tr("Localizable", "done")
+  internal static let done = Localization.tr("Localizable", "done", fallback: "Done")
   /// Email
-  internal static let email = Localization.tr("Localizable", "email")
-  /// Error
-  internal static let error = Localization.tr("Localizable", "error")
+  internal static let email = Localization.tr("Localizable", "email", fallback: "Email")
+  /// Localizable.strings
+  ///   MVVMSkeleton
+  /// 
+  ///   Created by Roman Savchenko on 12.12.2021.
+  internal static let error = Localization.tr("Localizable", "error", fallback: "Error")
   /// Home
-  internal static let home = Localization.tr("Localizable", "home")
+  internal static let home = Localization.tr("Localizable", "home", fallback: "Home")
   /// Logout
-  internal static let logout = Localization.tr("Localizable", "logout")
+  internal static let logout = Localization.tr("Localizable", "logout", fallback: "Logout")
   /// Name
-  internal static let name = Localization.tr("Localizable", "name")
+  internal static let name = Localization.tr("Localizable", "name", fallback: "Name")
   /// OK
-  internal static let ok = Localization.tr("Localizable", "ok")
+  internal static let ok = Localization.tr("Localizable", "ok", fallback: "OK")
   /// Password
-  internal static let password = Localization.tr("Localizable", "password")
+  internal static let password = Localization.tr("Localizable", "password", fallback: "Password")
   /// Settings
-  internal static let settings = Localization.tr("Localizable", "settings")
+  internal static let settings = Localization.tr("Localizable", "settings", fallback: "Settings")
   /// Sign In
-  internal static let signIn = Localization.tr("Localizable", "sign-in")
+  internal static let signIn = Localization.tr("Localizable", "sign-in", fallback: "Sign In")
   /// Sign Up
-  internal static let signUp = Localization.tr("Localizable", "sign-up")
+  internal static let signUp = Localization.tr("Localizable", "sign-up", fallback: "Sign Up")
   /// Skip
-  internal static let skip = Localization.tr("Localizable", "skip")
+  internal static let skip = Localization.tr("Localizable", "skip", fallback: "Skip")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -47,8 +50,8 @@ internal enum Localization {
 // MARK: - Implementation Details
 
 extension Localization {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
